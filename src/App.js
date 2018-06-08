ec3c52cf39f0573137726527f1c28d63c15032de
litesoul <exorcismo@gmail.com>
2018-06-08 12:19:43 -0300

    Sync

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+	render() {
+		return (
+			<div className="f1 tc">
+				<h2>This is app.js</h2>
+				<p>Not properly colored</p>
+			</div>
+		)
+	}
+}
+
+export default App
